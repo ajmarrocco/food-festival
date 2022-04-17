5273f54107820b9b406a16c348d05a23fbbff4d4
ajmarrocco <ajmarrocco@gmail.com>
2022-04-16 20:31:08 -0400

    updated packages per corrected versions

--- webpack.config.js
+++ webpack.config.js
@@ -1,54 +1,3 @@
-// const path = require("path");
-// const webpack = require("webpack");
-// const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
-
-// module.exports = {
-//     entry: {
-//         app: "./assets/js/script.js",
-//         events: "./assets/js/events.js",
-//         schedule: "./assets/js/schedule.js",
-//         tickets: "./assets/js/tickets.js"
-//     },
-//     output: {
-//         filename: "[name].bundle.js",
-//         path: __dirname + "/dist",
-//     },
-//     module: {
-//         rules: [
-//             {
-//                 test: /\.jpg$/,
-//                 use: [
-//                     {
-//                         loader: 'file-loader',
-//                         options: {
-//                             esModule: false,
-//                             name(file) {
-//                                 return '[path][name].[ext]';
-//                             },
-//                             publicPath: function(url) {
-//                                 return url.replace('../', '/assets/');
-//                             }
-//                         }
-//                     },
-//                     {
-//                         loader: 'image-webpack-loader'
-//                     }
-//                 ]
-//             }
-//         ]
-//     },
-//     plugins: [
-//         new webpack.ProvidePlugin({
-//             $: "jquery",
-//             jQuery: "jquery"
-//         }),
-//         new BundleAnalyzerPlugin({
-//             analyzerMode: "static", // the report outputs to an HTML file in the dist folder
-//         })
-//     ],
-//     mode: 'development',
-// };
-
 const webpack = require('webpack');
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 // const SWPrecacheWebpackPlugin = require("sw-precache-webpack-plugin");
@@ -113,10 +62,7 @@ const config = {
     //   }]
     // })
   ],
-  mode: 'development',
-  // devServer: {
-  //   static: "./dist",
-  // },
+  mode: 'development'
 };
 
 module.exports = config;
